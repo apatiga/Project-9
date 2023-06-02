a4d01a404ca8258d5c1f03296e89aba7f0aea63b
apatiga <128877342+apatiga@users.noreply.github.com>
2023-06-01 17:32:14 -0700

    added packages needed to run application

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs")
+const inquirer = require("inquirer")
+const path = require("path")
+const generateMarkdown = require("./utils/generateMarkdwon")
 // TODO: Create an array of questions for user input
 const questions = [];
 
