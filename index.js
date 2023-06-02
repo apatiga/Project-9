616a00e31cd022d0334e6dcd428fd1658a81289d
apatiga <128877342+apatiga@users.noreply.github.com>
2023-06-01 18:23:17 -0700

    added questions to input in the README generator

--- index.js
+++ index.js
@@ -4,13 +4,42 @@ const inquirer = require("inquirer")
 const path = require("path")
 const generateMarkdown = require("./utils/generateMarkdwon")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Plese name your project",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please describe the purpose and functionality of this project.",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        messgae: "Please select a liscense applicable to the project",
+        choices: "something",
+    },
+
+    {
+        
+
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd().fileName),data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.createPromptModule(questions).then(responses) -> (
+        console.log("Creating Professional README.md File...");
+        writeToFile("./dist/README.md" , generateMarkdown({ ...responses }))
+    );
+}
 
 // Function call to initialize app
 init();
